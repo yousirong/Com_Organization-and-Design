5a0c2e1e28982d81ee92f064ef6be87fc6ae8ef3
yousirong <80014277+yousirong@users.noreply.github.com>
2022-03-12 01:15:00 +0900

    20220312

--- com_OandD_proejct/project_1.c
+++ com_OandD_proejct/project_1.c
@@ -6,5 +6,14 @@ void testOperator(int x, int y){
 
 int main(void)
 {
-    return 0;
+	int a,b,c;
+	a = 0x01;
+	b = 0x03;
+	c = 0x00;
+
+	c = a&b;
+
+	printf("0x%08x & 0x%08x = 0x%08x \n",a,b,c);
+
+	return 0;
 }
