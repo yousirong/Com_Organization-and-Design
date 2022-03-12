2493bf99dc8d217422d93f3912be4577c46c5276
yousirong <80014277+yousirong@users.noreply.github.com>
2022-03-12 16:26:28 +0900

    20220312

--- com_OandD_proejct/project_1.c
+++ com_OandD_proejct/project_1.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h> // rand() 함수 포함 라이브러리
 
 void testOperator(int x, int y){
-
+    int ans = x&y;
+    printf("0x%08x & 0x%08x = 0x%08x \n",x,y,ans);
+    int res = ans << 2;
+    printf("0x%08x << 2 = 0x%08x",ans,res);
 }
 
 int main(void)
 {
-	int a,b,c;
-	a = 0x01;
-	b = 0x03;
-	c = 0x00;
-
-	c = a&b;
-
-	printf("0x%08x & 0x%08x = 0x%08x \n",a,b,c);
-
+	int x,y;
+    for(int i =0; i<5; i++){ //5번 반복
+        x = rand()%9;
+        y = rand()%9;
+    }
+    testOperator(x,y);
 	return 0;
 }
