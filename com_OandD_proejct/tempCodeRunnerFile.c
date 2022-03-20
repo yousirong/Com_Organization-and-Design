047bfb3595e90db2c2d3f17b0896bd7481b4bf5c
yousirong <80014277+yousirong@users.noreply.github.com>
2022-03-20 23:47:17 +0900

    20220320

---
+++ com_OandD_proejct/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+ while (1) {
+//     count = fread(&data, sizeof(data1), 1, qFile);
+//     if (count  != 1)
+//       break;
+//     printf("%8x\n", invertEndian(data));
+//   }
+//   fwrite(&data, sizeof(data1), 1, qFile);
