84a212d0297306853af277a5500837043b625fd8
yousirong <diziyong1523@gmail.com>
2022-03-26 02:22:13 +0900

    20220326

--- com_OandD_proejct/project_3.c
+++ com_OandD_proejct/project_3.c
@@ -1,24 +1,29 @@
 #define is_bigendian() ( (*(char*)&i) == 0 )
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
+const int M_SIZE =1024;
+unsigned char MEM[1024];
 const int i = 1;
-int invertEndian (int inputValue) {  //endian이 바뀐 데이터를 반환하는 함수
+int memoryRead (int addr) {
     unsigned char i1, i2, i3, i4;
     if (is_bigendian()) {
-        return inputValue;
+        return addr;
         } else {
-            i1 = inputValue & 255;
-            i2 = (inputValue >> 8) & 255;
-            i3 = (inputValue >> 16) & 255;
-            i4 = (inputValue >> 24) & 255;
+            i1 = addr & 255;
+            i2 = (addr >> 8) & 255;
+            i3 = (addr >> 16) & 255;
+            i4 = (addr >> 24) & 255;
             return ((int)i1 << 24) + ((int)i2 << 16) + ((int)i3 << 8) + i4;
 }
 }
 
+void memoryWrite(unsigned int addr, unsigned int data){
+
+}
 int main(void){
 
   FILE *pFile = fopen("input.bin", "rb");
-  FILE *qFile = fopen("output.bin","wb");
+
 
   int count;
   unsigned int data;
@@ -31,23 +36,17 @@ int main(void){
     puts("input파일 오픈 실패!");
     return -1;
     }
-  if (qFile==NULL)
-  {
-    puts("output파일 오픈 실패!");
-    return -1;
-    }
 
     while (1) {
     count = fread(&data, sizeof(data1), 1, pFile);
     if (count != 1)
       break;
-    //printf("%08x\n", data);
-    exdata = invertEndian(data);   // input.bin에서 읽은 데이터를 endian이 바뀐 데이터를 반환하는 함수를 사용
-    resdata = invertEndian(data)^resdata; //출력파일에 저장할 19개의 데이터를 모두 XOR
-    fwrite(&exdata, sizeof(data1), 1, qFile);  //output.bin에 fwrite
+
+    exdata = memoryRead(data);
+    memoryWrite(MEM ,exdata);
+    printf("%08x\n", exdata);
   }
-  printf("%08x\n",resdata); //출력파일에 저장할 19개의 데이터를 모두 XOR하여 그 값을 Hexa로 출력
-  fclose(qFile);
+
   fclose(pFile);
   return 0;
 }
