305e82a3d66c36aa131c6b9d64b2dd1ee36d6127
yousirong <diziyong1523@gmail.com>
2022-03-27 03:37:05 +0900

    20220327

--- com_OandD_proejct/project_3.c
+++ com_OandD_proejct/project_3.c
@@ -2,33 +2,55 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 const int M_SIZE =1024;
-unsigned char MEM[1024];
+unsigned int MEM[1024];
 const int i = 1;
-int memoryRead (int addr) {
+
+
+int invertEndian (int inputValue) {  //endian이 바뀐 데이터를 반환하는 함수
     unsigned char i1, i2, i3, i4;
     if (is_bigendian()) {
-        return addr;
+        return inputValue;
         } else {
-            i1 = addr & 255;
-            i2 = (addr >> 8) & 255;
-            i3 = (addr >> 16) & 255;
-            i4 = (addr >> 24) & 255;
+            i1 = inputValue & 255;
+            i2 = (inputValue >> 8) & 255;
+            i3 = (inputValue >> 16) & 255;
+            i4 = (inputValue >> 24) & 255;
             return ((int)i1 << 24) + ((int)i2 << 16) + ((int)i3 << 8) + i4;
 }
 }
 
-void memoryWrite(unsigned int addr, unsigned int data){
+unsigned int memoryRead(unsigned int addr)
+{
 
 }
+void memoryWrite(unsigned int addr, unsigned int data)
+{
+    unsigned char i1, i2, i3, i4;
+    if (is_bigendian()) {
+        return data;
+        } else {
+            i1 = data & 255;
+            i2 = (data >> 8) & 255;
+            i3 = (data >> 16) & 255;
+            i4 = (data >> 24) & 255;
+            // printf("%02x %02x %02x %02x\n",i1, i2,i3,i4);
+            MEM[addr] = i1;
+            MEM[addr+1] = i2;
+            MEM[addr+2] = i3;
+            MEM[addr+3] =i4;
+            printf("%02x %02x %02x %02x\n",MEM[addr], MEM[addr+1], MEM[addr+2], MEM[addr+3]);
+            // return ((int)i1 << 24) + ((int)i2 << 16) + ((int)i3 << 8) + i4;
+            // return addr[0], addr[2], addr[4], addr[6];
+}
+}
+
 int main(void){
-
   FILE *pFile = fopen("input.bin", "rb");
-
-
   int count;
   unsigned int data;
   unsigned int exdata;
-  unsigned int resdata; // 출력파일에 저장할 19개의 데이터를 모두 XOR하여 그 값을 Hexa로 출력하는 변수
+  int cnt=0;
+//   unsigned int resdata; // 출력파일에 저장할 19개의 데이터를 모두 XOR하여 그 값을 Hexa로 출력하는 변수
   unsigned int data1 = 0xAABBCCDD;
   unsigned int data2 = 0x11223344;
   if (pFile==NULL)
@@ -42,9 +64,10 @@ int main(void){
     if (count != 1)
       break;
 
-    exdata = memoryRead(data);
-    memoryWrite(MEM ,exdata);
-    printf("%08x\n", exdata);
+    exdata = invertEndian(data);
+    printf("%08x  %d\n", exdata, cnt);
+    memoryWrite(cnt,data);
+    cnt++;
   }
 
   fclose(pFile);
