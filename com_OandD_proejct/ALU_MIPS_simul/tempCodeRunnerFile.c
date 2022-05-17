b7f1f1d5d2e2ccd6d2bd1bb45ce616baa22d8e32
yousirong <80014277+yousirong@users.noreply.github.com>
2022-05-17 18:32:47 +0900

    20220517

--- com_OandD_proejct/ALU_MIPS_simul/tempCodeRunnerFile.c
+++ com_OandD_proejct/ALU_MIPS_simul/tempCodeRunnerFile.c
@@ -1,1 +1,35 @@
-i
+case 32: {
+			// add
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 8, &Z);
+			break; }
+		case 34: {
+			// sub
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 9, &Z);
+			break; }
+		case 36: {
+			// and
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 12, &Z);
+			break; }
+		case 37: {
+			// or
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 13, &Z);
+			break; }
+		case 38: {
+			// xor
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 14, &Z);
+			break; }
+		case 39: {
+			// nor
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 15, &Z);
+			break; }
+		case 42: {
+			// slt
+			int Z;
+			R[IR.RI.rd] = ALU(R[IR.RI.rs], R[IR.RI.rt], 4, &Z);
+			break; }
